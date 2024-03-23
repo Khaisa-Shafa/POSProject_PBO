e8dad9807d5df8114763438ff04b0abaf498f5e0
VIVOBOOK <VIVOBOOK@KHAISA>
2024-03-23 20:51:03 +0700

    first commit

---
+++ src/posproject/LoginPage.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package posproject;
+
+/**
+ *
+ * @author VIVOBOOK
+ */
+public class LoginPage {
+    
+    public static void main(String[] args) {
+        POSFrame_login log = new POSFrame_login();
+        log.show();
+    }
+    
+}
